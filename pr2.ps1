7efcc66390c0e16fac6a6c5fa41945bc00734e15
Armand843 <armand.peetersoo@voco.ee>
2023-11-16 09:06:52 +0000

    Praktika 2

---
+++ pr2.ps1
@@ -0,0 +1,20 @@
+﻿$hashtable1 = @{
+    Name = @("John","Joe","Mary")
+    DaysWorked = @(12, 20, 18)
+}
+
+$hashtable2 = @{
+    Name = @("John","Joe","Mary")
+    SalaryPerDay = @(100, 120, 150)
+}
+
+$JohnSalary = $hash1.DaysWorked[0] * $hash2.SalaryPerDay[0]
+$JoeSalary = $hash1.DaysWorked[1] * $hash2.SalaryPerDay[1]
+$MarySalary = $hash1.DaysWorked[2] * $hash2.SalaryPerDay[2]
+
+$hashtable3 = @{
+    Name = $hash1.Name[0], $hash1.Name[1], $hash1.Name[2]
+    Salary = $JohnSalary, $JoeSalary, $MarySalary
+}
+
+$hashtable3
