0a04221fd675d6acad26e6ca520288b64887c196
Armand843 <armand.peetersoo@voco.ee>
2023-11-23 10:05:06 +0200

    Lokaalse kasutaja kustutamise ülesanne

---
+++ kasutajadyl2.ps1
@@ -0,0 +1,14 @@
+﻿# 1. Küsi kasutajalt ees- ja perenimi, keda kustutada
+$eesnimiKustutamine = Read-Host "Sisesta kasutaja eesnimi, keda kustutada"
+$perenimiKustutamine = Read-Host "Sisesta kasutaja perenimi, keda kustutada"
+
+# 2. Loo kasutajanimi
+$kasutajanimiKustutamine = "$eesnimiKustutamine.$perenimiKustutamine"
+
+# 3. Kustuta kasutaja vastavalt loodud kasutajanimele
+try {
+    Remove-LocalUser -Name $kasutajanimiKustutamine -ErrorAction Stop
+    Write-Host "Kasutaja $kasutajanimiKustutamine kustutatud edukalt."
+} catch {
+    Write-Host "Kasutaja kustutamine ebaõnnestus. Veateade: $($_.Exception.Message)"
+}
