7593f50fa6acf9d404de22683123cac724ecbb69
Armand843 <armand.peetersoo@voco.ee>
2023-11-23 10:04:09 +0200

    Lokaalse kasutaja loomise ülesanne

---
+++ kasutajadyl1.ps1
@@ -0,0 +1,15 @@
+﻿$eesnimi = Read-Host "Sisesta eesnimi"
+$perenimi = Read-Host "Sisesta perenimi"
+
+$kasutajanimi = "$eesnimi.$perenimi"
+$täisnimi = "$eesnimi $perenimi"
+$kontoKirjeldus = "Kasutaja loodud PowerShell skriptiga"
+
+$parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+$kasutajaLoomine = New-LocalUser -Name $kasutajanimi -Password $parool -Description $kontoKirjeldus
+
+if ($?) {
+    Write-Host "Kasutaja $kasutajanimi loodud edukalt."
+} else {
+    Write-Host "Kasutaja loomine ebaõnnestus. Veateade: $($Error[0].Exception.Message)"
+}
