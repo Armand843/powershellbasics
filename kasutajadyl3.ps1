d9e11c6b0b5ec52247892cd0cc517a001057f557
Armand843 <armand.peetersoo@voco.ee>
2023-11-30 11:11:05 +0200

    AD kasutajate loomis skript

---
+++ kasutajadyl3.ps1
@@ -0,0 +1,33 @@
+﻿$eesnimi = Read-Host "Sisesta eesnimi"
+$perenimi = Read-Host "Sisesta perenimi"
+
+function Translit {
+    param (
+        [string]$text
+    )
+
+    $translitTable = @{
+        'ä' = 'a'
+        'ö' = 'o'
+        'õ' = '6'
+        'ü' = 'u'
+        
+    }
+
+    $translitText = $text.ToLower()
+
+    foreach ($char in $translitTable.Keys) {
+        $translitText = $translitText -replace $char, $translitTable[$char]
+    }
+
+    return $translitText
+}
+
+$kasutajanimi = Translit -text "$eesnimi.$perenimi"
+
+try {
+    Remove-ADUser -Identity $kasutajanimi -Confirm:$false -ErrorAction Stop
+    Write-Host "Kasutaja $kasutajanimi edukalt kustutatud AD-st."
+} catch {
+    Write-Host "Kasutaja kustutamine ebaõnnestus. Veateade: $($_.Exception.Message)"
+}
